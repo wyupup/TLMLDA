ee53e9438b95431a36061fe0eeaedf93e1817b7d
wyupup <46292164+wyupup@users.noreply.github.com>
2022-01-09 19:02:45 +0800

    Add files via upload

---
+++ TLMLDA_M/mmd.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Dec  9 10:35:10 2020
+
+@author: ZHUANG
+"""
+import torch
+
+def guassian_kernel(source, target, kernel_mul=2.0, kernel_num=5, fix_sigma=None):
+    n_samples = int(source.size()[0])+int(target.size()[0])  
+    total = torch.cat([source, target], dim=0) 
+    total0 = total.unsqueeze(0).expand(int(total.size(0)), int(total.size(0)), int(total.size(1)))
+    total1 = total.unsqueeze(1).expand(int(total.size(0)), int(total.size(0)), int(total.size(1)))
+    L2_distance = ((total0-total1)**2).sum(2)
+    if fix_sigma:
+        bandwidth = fix_sigma
+    else:
+        bandwidth = torch.sum(L2_distance.data) / (n_samples**2-n_samples)
+    bandwidth /= kernel_mul ** (kernel_num // 2)
+    bandwidth_list = [bandwidth * (kernel_mul**i) for i in range(kernel_num)]
+
+    kernel_val = [torch.exp(-L2_distance / bandwidth_temp) for bandwidth_temp in bandwidth_list] 
+    return sum(kernel_val) 
+                                                                                                         
+def mmd_rbf(source, target, kernel_mul=2.0, kernel_num=5, fix_sigma=None):    
+    batch_size = source.size()[0]  
+    kernels = guassian_kernel(source, target,
+                              kernel_mul=kernel_mul, kernel_num=kernel_num, fix_sigma=fix_sigma)
+
+    loss = torch.Tensor([0]).cuda()
+    if torch.sum(torch.isnan(sum(kernels))):  #torch.isnan()来检查tensor数据中是否存在脏数据
+        return loss
+    SS = kernels[:batch_size, :batch_size]
+    TT = kernels[batch_size:, batch_size:]
+    ST = kernels[:batch_size, batch_size:]
+    
+    loss = torch.mean(SS + TT - 2 * ST)
+    return loss
